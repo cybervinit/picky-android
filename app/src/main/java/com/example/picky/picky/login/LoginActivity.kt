f6f933e81b423600df798f2f6ae66985aeefcc4f
Vinit Soni <vinitsoni9869@gmail.com>
2018-09-29 13:01:57 -0400

    add dynamic font sizing in usernameEt during login

--- app/src/main/java/com/example/picky/picky/login/LoginActivity.kt
+++ app/src/main/java/com/example/picky/picky/login/LoginActivity.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.DisplayMetrics
 import android.util.Log
 import android.widget.Toast
 import com.example.picky.picky.PickyApplication
@@ -17,13 +20,13 @@ import com.example.picky.picky.login.interfacing.ILoginView
 import com.example.picky.picky.signup.SignupActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 class LoginActivity : AppCompatActivity(), ILoginView {
 
     private val NEW_USER_REQUEST_CODE: Int = 1 // 1: Request code to return username and password from the SignupActivity
     private val ACTION_NEW_USER = "com.example.picky.picky.action.NEW_USER"
     private val NEW_USERNAME_KEY = "new_username"
-    private val NEW_PASSWORD_KEY = "new_password"
 
     @Inject lateinit var loginPresenter: ILoginPresenter.forView
 
@@ -52,6 +55,22 @@ class LoginActivity : AppCompatActivity(), ILoginView {
 
         context = this
 
+        val displayMetrics: DisplayMetrics = DisplayMetrics()
+        (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        usernameEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                val usernameLength = if (usernameEt.text.toString().length <= 8) 8 else p0.toString().length
+                usernameEt.textSize = ((displayMetrics.widthPixels * 0.65f)*0.192*8/(usernameLength*3)).toFloat()
+                // Explanation: used screen width to figure out width of usernameEt multiplied by
+                // 0.192 (= 135 (initial font size)/ 702 (samsung s7 usernameEt width))
+                // times (8 ('username')/(total letters in username)) scaled down by 1/3
+            }
+
+        })
 
         signUpBtn.setOnClickListener {
             startActivityForResult(Intent(this, SignupActivity::class.java), NEW_USER_REQUEST_CODE)
@@ -63,7 +82,6 @@ class LoginActivity : AppCompatActivity(), ILoginView {
         if (requestCode == NEW_USER_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
                 usernameEt.setText(data?.getStringExtra(NEW_USERNAME_KEY))
-                passwordEt.setText(data?.getStringExtra(NEW_PASSWORD_KEY))
                 Toast.makeText(applicationContext, "Time to login with the creds!", Toast.LENGTH_SHORT).show() // FIXME: remove Toast?
                 // TODO: login()
             } else if (resultCode == Activity.RESULT_CANCELED) {
